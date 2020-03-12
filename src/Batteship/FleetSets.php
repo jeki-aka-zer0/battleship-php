1ff10ed267c8d8a76f2c8eefb7255566f002632f
Алла Панченко <evetrueivory@gmail.com>
2020-03-12 17:35:07 +0300

    Merge branch 'master' of https://github.com/jeki-aka-zer0/battleship-php
    # Conflicts:
    #	src/App.php

--- src/Batteship/FleetSets.php
+++ src/Batteship/FleetSets.php
@@ -79,9 +79,9 @@ return array(
         'Destroyer' => [
             'color' => Color::YELLOW,
             'positions' => [
-                ['G', 2],
                 ['H', 2],
                 ['I', 2],
+                ['J', 2],
             ]
         ],
         'Patrol Boat' => [
@@ -98,43 +98,43 @@ return array(
         "Aircraft Carrier" => [
             'color' => Color::CADET_BLUE,
             'positions' => [
-                ['B', 1],
-                ['B', 2],
-                ['B', 3],
-                ['B', 4],
-                ['B', 5],
+                ['A', 10],
+                ['B', 10],
+                ['C', 10],
+                ['D', 10],
+                ['E', 10],
             ]
         ],
         'Battleship' => [
             'color' => Color::RED,
             'positions' => [
-                ['E', 4],
-                ['F', 4],
-                ['G', 4],
-                ['H', 4],
+                ['D', 7],
+                ['E', 7],
+                ['F', 7],
+                ['G', 7],
             ]
         ],
         'Submarine' => [
             'color' => Color::CHARTREUSE,
             'positions' => [
-                ['C', 8],
-                ['C', 9],
-                ['C', 10],
+                ['A', 3],
+                ['A', 4],
+                ['A', 5],
             ]
         ],
         'Destroyer' => [
             'color' => Color::YELLOW,
             'positions' => [
-                ['E', 8],
-                ['E', 9],
-                ['E', 10],
+                ['J', 5],
+                ['J', 6],
+                ['J', 7],
             ]
         ],
         'Patrol Boat' => [
             'color' => Color::ORANGE,
             'positions' => [
-                ['I', 6],
-                ['I', 7],
+                ['G', 3],
+                ['H', 3],
             ]
         ]
     ],
@@ -144,43 +144,43 @@ return array(
         "Aircraft Carrier" => [
             'color' => Color::CADET_BLUE,
             'positions' => [
-                ['B', 1],
-                ['B', 2],
-                ['B', 3],
-                ['B', 4],
-                ['B', 5],
+                ['D', 9],
+                ['E', 9],
+                ['F', 9],
+                ['G', 9],
+                ['H', 9],
             ]
         ],
         'Battleship' => [
             'color' => Color::RED,
             'positions' => [
-                ['E', 4],
-                ['F', 4],
-                ['G', 4],
-                ['H', 4],
+                ['I', 5],
+                ['I', 6],
+                ['I', 7],
+                ['I', 8],
             ]
         ],
         'Submarine' => [
             'color' => Color::CHARTREUSE,
             'positions' => [
-                ['C', 8],
-                ['C', 9],
-                ['C', 10],
+                ['A', 6],
+                ['A', 7],
+                ['A', 8],
             ]
         ],
         'Destroyer' => [
             'color' => Color::YELLOW,
             'positions' => [
-                ['E', 8],
-                ['E', 9],
-                ['E', 10],
+                ['C', 4],
+                ['C', 5],
+                ['C', 6],
             ]
         ],
         'Patrol Boat' => [
             'color' => Color::ORANGE,
             'positions' => [
-                ['I', 6],
-                ['I', 7],
+                ['D', 1],
+                ['E', 1],
             ]
         ]
     ],
@@ -190,43 +190,43 @@ return array(
         "Aircraft Carrier" => [
             'color' => Color::CADET_BLUE,
             'positions' => [
-                ['B', 1],
-                ['B', 2],
-                ['B', 3],
-                ['B', 4],
-                ['B', 5],
+                ['A', 2],
+                ['A', 3],
+                ['A', 4],
+                ['A', 5],
+                ['A', 6],
             ]
         ],
         'Battleship' => [
             'color' => Color::RED,
             'positions' => [
-                ['E', 4],
-                ['F', 4],
-                ['G', 4],
-                ['H', 4],
+                ['C', 4],
+                ['C', 5],
+                ['C', 6],
+                ['C', 7],
             ]
         ],
         'Submarine' => [
             'color' => Color::CHARTREUSE,
             'positions' => [
-                ['C', 8],
-                ['C', 9],
-                ['C', 10],
+                ['E', 2],
+                ['F', 2],
+                ['G', 2],
             ]
         ],
         'Destroyer' => [
             'color' => Color::YELLOW,
             'positions' => [
-                ['E', 8],
-                ['E', 9],
-                ['E', 10],
+                ['E', 4],
+                ['F', 4],
+                ['G', 4],
             ]
         ],
         'Patrol Boat' => [
             'color' => Color::ORANGE,
             'positions' => [
-                ['I', 6],
-                ['I', 7],
+                ['F', 6],
+                ['G', 6],
             ]
         ]
     ],
@@ -236,43 +236,43 @@ return array(
         "Aircraft Carrier" => [
             'color' => Color::CADET_BLUE,
             'positions' => [
-                ['B', 1],
-                ['B', 2],
-                ['B', 3],
-                ['B', 4],
-                ['B', 5],
+                ['A', 7],
+                ['B', 7],
+                ['C', 7],
+                ['D', 7],
+                ['E', 7],
             ]
         ],
         'Battleship' => [
             'color' => Color::RED,
             'positions' => [
+                ['E', 2],
+                ['E', 3],
                 ['E', 4],
-                ['F', 4],
-                ['G', 4],
-                ['H', 4],
+                ['E', 5],
             ]
         ],
         'Submarine' => [
             'color' => Color::CHARTREUSE,
             'positions' => [
-                ['C', 8],
-                ['C', 9],
-                ['C', 10],
+                ['J', 3],
+                ['H', 3],
+                ['I', 3],
             ]
         ],
         'Destroyer' => [
             'color' => Color::YELLOW,
             'positions' => [
-                ['E', 8],
-                ['E', 9],
-                ['E', 10],
+                ['G', 9],
+                ['H', 9],
+                ['I', 9],
             ]
         ],
         'Patrol Boat' => [
             'color' => Color::ORANGE,
             'positions' => [
+                ['H', 6],
                 ['I', 6],
-                ['I', 7],
             ]
         ]
     ],
@@ -282,43 +282,43 @@ return array(
         "Aircraft Carrier" => [
             'color' => Color::CADET_BLUE,
             'positions' => [
-                ['B', 1],
-                ['B', 2],
-                ['B', 3],
-                ['B', 4],
-                ['B', 5],
+                ['D', 6],
+                ['D', 7],
+                ['D', 8],
+                ['D', 9],
+                ['D', 10],
             ]
         ],
         'Battleship' => [
             'color' => Color::RED,
             'positions' => [
-                ['E', 4],
-                ['F', 4],
-                ['G', 4],
-                ['H', 4],
+                ['C', 2],
+                ['C', 3],
+                ['C', 4],
+                ['C', 5],
             ]
         ],
         'Submarine' => [
             'color' => Color::CHARTREUSE,
             'positions' => [
-                ['C', 8],
-                ['C', 9],
-                ['C', 10],
+                ['E', 2],
+                ['F', 2],
+                ['G', 2],
             ]
         ],
         'Destroyer' => [
             'color' => Color::YELLOW,
             'positions' => [
-                ['E', 8],
-                ['E', 9],
-                ['E', 10],
+                ['H', 8],
+                ['H', 9],
+                ['H', 10],
             ]
         ],
         'Patrol Boat' => [
             'color' => Color::ORANGE,
             'positions' => [
-                ['I', 6],
-                ['I', 7],
+                ['F', 5],
+                ['F', 6],
             ]
         ]
     ],
@@ -328,43 +328,43 @@ return array(
         "Aircraft Carrier" => [
             'color' => Color::CADET_BLUE,
             'positions' => [
-                ['B', 1],
-                ['B', 2],
-                ['B', 3],
-                ['B', 4],
-                ['B', 5],
+                ['I', 6],
+                ['I', 7],
+                ['I', 8],
+                ['I', 9],
+                ['I', 10],
             ]
         ],
         'Battleship' => [
             'color' => Color::RED,
             'positions' => [
-                ['E', 4],
-                ['F', 4],
                 ['G', 4],
-                ['H', 4],
+                ['G', 5],
+                ['G', 6],
+                ['G', 7],
             ]
         ],
         'Submarine' => [
             'color' => Color::CHARTREUSE,
             'positions' => [
-                ['C', 8],
-                ['C', 9],
-                ['C', 10],
+                ['D', 8],
+                ['D', 9],
+                ['D', 10],
             ]
         ],
         'Destroyer' => [
             'color' => Color::YELLOW,
             'positions' => [
-                ['E', 8],
-                ['E', 9],
-                ['E', 10],
+                ['A', 2],
+                ['B', 2],
+                ['C', 2],
             ]
         ],
         'Patrol Boat' => [
             'color' => Color::ORANGE,
             'positions' => [
-                ['I', 6],
-                ['I', 7],
+                ['D', 4],
+                ['D', 5],
             ]
         ]
     ],
@@ -374,43 +374,43 @@ return array(
         "Aircraft Carrier" => [
             'color' => Color::CADET_BLUE,
             'positions' => [
-                ['B', 1],
-                ['B', 2],
-                ['B', 3],
-                ['B', 4],
-                ['B', 5],
+                ['F', 10],
+                ['G', 10],
+                ['H', 10],
+                ['I', 10],
+                ['J', 10],
             ]
         ],
         'Battleship' => [
             'color' => Color::RED,
             'positions' => [
-                ['E', 4],
-                ['F', 4],
-                ['G', 4],
-                ['H', 4],
+                ['E', 5],
+                ['E', 6],
+                ['E', 7],
+                ['E', 8],
             ]
         ],
         'Submarine' => [
             'color' => Color::CHARTREUSE,
             'positions' => [
-                ['C', 8],
-                ['C', 9],
-                ['C', 10],
+                ['C', 4],
+                ['C', 5],
+                ['C', 6],
             ]
         ],
         'Destroyer' => [
             'color' => Color::YELLOW,
             'positions' => [
-                ['E', 8],
-                ['E', 9],
-                ['E', 10],
+                ['D', 1],
+                ['E', 1],
+                ['F', 1],
             ]
         ],
         'Patrol Boat' => [
             'color' => Color::ORANGE,
             'positions' => [
-                ['I', 6],
-                ['I', 7],
+                ['A', 4],
+                ['A', 5],
             ]
         ]
     ],
@@ -420,43 +420,43 @@ return array(
         "Aircraft Carrier" => [
             'color' => Color::CADET_BLUE,
             'positions' => [
-                ['B', 1],
-                ['B', 2],
-                ['B', 3],
-                ['B', 4],
-                ['B', 5],
+                ['F', 4],
+                ['G', 4],
+                ['H', 4],
+                ['I', 4],
+                ['J', 4],
             ]
         ],
         'Battleship' => [
             'color' => Color::RED,
             'positions' => [
-                ['E', 4],
-                ['F', 4],
-                ['G', 4],
-                ['H', 4],
+                ['D', 7],
+                ['D', 8],
+                ['D', 9],
+                ['D', 10],
             ]
         ],
         'Submarine' => [
             'color' => Color::CHARTREUSE,
             'positions' => [
-                ['C', 8],
-                ['C', 9],
-                ['C', 10],
+                ['F', 7],
+                ['F', 8],
+                ['F', 9],
             ]
         ],
         'Destroyer' => [
             'color' => Color::YELLOW,
             'positions' => [
-                ['E', 8],
-                ['E', 9],
-                ['E', 10],
+                ['A', 1],
+                ['B', 1],
+                ['C', 1],
             ]
         ],
         'Patrol Boat' => [
             'color' => Color::ORANGE,
             'positions' => [
-                ['I', 6],
-                ['I', 7],
+                ['D', 3],
+                ['D', 4],
             ]
         ]
     ],
